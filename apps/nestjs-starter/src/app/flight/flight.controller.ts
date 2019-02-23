4165f6f8c9ea73582b8b82d6dd348c31e08dd431
Marco <m.haemmerle@nemocontra.de>
2019-02-23 17:23:21 +0100

    final service labs

--- apps/nestjs-starter/src/app/flight/flight.controller.ts
+++ apps/nestjs-starter/src/app/flight/flight.controller.ts
@@ -1,41 +1,44 @@
-import { Controller, Get, Query, Post, Body, Param, Delete, HttpCode, HttpException } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Param,
+  Delete,
+  HttpCode,
+  NotFoundException
+} from '@nestjs/common';
 import { Flight } from '@flight-app/shared';
-import flights from '../../../mock-data/flights.json';
+import { FlightService } from './flight.service';
 
 @Controller('flight')
 export class FlightController {
-    private flights: Flight[] = flights;
+
+    constructor(private flightService: FlightService) {
+    }
 
     @Get()
     getFlights(@Query('from') from: string, @Query('to') to: string): Flight[] {
-        return this.flights.filter(flight => flight.from === from && flight.to === to)
+        return this.flightService.searchFlights(from, to);
     }
 
     @Get(':id')
     getFlightById(@Param('id') id: string): Flight {
-      return this.flights.find(flight => flight.id === +id);
+      return this.flightService.getFlightById(+id);
     }
 
     @Post()
     @HttpCode(201)
     createFlight(@Body() flight: Flight): Flight {
-      const newFlight: Flight = {
-        ...flight,
-        id: this.flights.length + 1
-      };
-      this.flights = [
-        ...this.flights,
-        newFlight
-      ];
-      return newFlight;
+      return this.flightService.createFlight(flight);
     }
 
     @Delete(':id')
     deleteFlightById(@Param('id') id: string): void {
-      const length = this.flights.length;
-      this.flights = this.flights.filter(flight => flight.id !== +id);
-      if (length === this.flights.length) {
-        throw new HttpException('Flight not found.', 404);
+      const deleted = this.flightService.deleteFlight(+id);
+      if (!deleted) {
+        throw new NotFoundException('Flight not found.');
       }
     }
 }
