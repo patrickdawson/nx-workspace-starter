8bca60408a40de63b7b338df294062373a0f56da
Marco <m.haemmerle@nemocontra.de>
2019-02-23 17:23:21 +0100

    final service labs

--- apps/nestjs-starter/src/app/app.module.ts
+++ apps/nestjs-starter/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlightController } from './flight/flight.controller';
+import { FlightService } from './flight/flight.service';
 
 
 @Module({
   imports: [],
   controllers: [AppController, FlightController],
-  providers: [AppService],
+  providers: [AppService, FlightService],
 })
 export class AppModule {}
