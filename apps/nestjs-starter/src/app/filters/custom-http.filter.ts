682321761eb8597c6e8b5af5e3b8017a3371a179
Marco <m.haemmerle@nemocontra.de>
2019-03-03 13:10:51 +0100

    final expcetion filter labs

---
+++ apps/nestjs-starter/src/app/filters/custom-http.filter.ts
@@ -0,0 +1,10 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+
+@Catch(HttpException)
+export class CustomHttpFilter<T> implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const http = host.switchToHttp();
+    const status = exception.getStatus();
+    http.getResponse().status(status).json({message: 'Custom message!'});
+  }
+}
