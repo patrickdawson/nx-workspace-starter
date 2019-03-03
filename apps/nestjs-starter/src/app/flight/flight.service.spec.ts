f349ff7c5ef64ce277388648a1b02a5d88c16384
Marco <m.haemmerle@nemocontra.de>
2019-03-03 15:33:09 +0100

    final http client labs

--- apps/nestjs-starter/src/app/flight/flight.service.spec.ts
+++ apps/nestjs-starter/src/app/flight/flight.service.spec.ts
@@ -1,14 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FlightService } from './flight.service';
+import { HttpModule, HttpService } from '@nestjs/common';
+import { of, throwError } from 'rxjs';
+import { Flight } from '@flight-app/shared';
 
 describe('FlightService', () => {
   let service: FlightService;
+  let httpService: HttpService;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      modules: [HttpModule],
       providers: [FlightService]
     }).compile();
     service = module.get<FlightService>(FlightService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should return an empty array if nothing is passed in "searchFlights"', () => {
@@ -49,13 +55,28 @@ describe('FlightService', () => {
   });
 
   it('should return correct flight for "getFlightById"', () => {
-    expect(service.getFlightById(4)).toEqual({
+    const mockData: Flight = {
       id: 4,
       from: 'Hamburg',
       to: 'Graz',
       date: '2019-02-22T09:07:54.1624336+00:00',
       delayed: false
+    };
+
+    spyOn(httpService, 'get').and.returnValue(of({ data: mockData }));
+
+    service.getFlightById(4).subscribe(data => expect(data).toEqual(mockData));
+    expect(httpService.get).toHaveBeenCalledWith('http://www.angular.at/api/flight/4');
+  });
+
+  it('should handle a HTTP-Error correctly for "getFlightById"', (done) => {
+    spyOn(httpService, 'get').and.returnValue(throwError({ response: { status: 400 } }));
+
+    service.getFlightById(4).subscribe(fail, error => {
+      expect(error.status).toEqual(400);
+      done();
     });
+    expect(httpService.get).toHaveBeenCalledWith('http://www.angular.at/api/flight/4');
   });
 
   it('should return true for "deleteFlight"', () => {
