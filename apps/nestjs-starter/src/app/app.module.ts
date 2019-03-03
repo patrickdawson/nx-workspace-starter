d578c256578da65005fb755c1f565e3e8ff3edcb
Marco <m.haemmerle@nemocontra.de>
2019-03-03 15:33:09 +0100

    final http client labs

--- apps/nestjs-starter/src/app/app.module.ts
+++ apps/nestjs-starter/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlightModule } from './flight/flight.module';
 import { PassengerModule } from './passenger/passenger.module';
+import { CoreModule } from './core/core.module';
 
 
 @Module({
-  imports: [FlightModule, PassengerModule],
+  imports: [FlightModule, PassengerModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/nestjs-starter/src/app/middleware/user.middleware.spec.ts
+++ apps/nestjs-starter/src/app/middleware/user.middleware.spec.ts
@@ -4,37 +4,26 @@ import { UserMiddleware } from './user.middleware';
 describe('UserMiddleware', () => {
   let middleware: UserMiddleware;
 
+  const user = {
+    id: 42,
+    username: 'testuser',
+    firstname: 'test',
+    lastname: 'user',
+    role: 'admin'
+  };
+
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserMiddleware]
+      providers: [UserMiddleware, { provide: 'USER', useValue: user }]
     }).compile();
     middleware = module.get<UserMiddleware>(UserMiddleware);
   });
 
   it('should set the user to the request object', async () => {
     const request: any = {};
-    const user = {
-      id: 42,
-      username: 'testuser',
-      firstname: 'test',
-      lastname: 'user',
-      role: 'admin'
-    };
 
-    const middlewarefunction = middleware.resolve(user);
-
-    middlewarefunction(request, undefined, () => {
+    middleware.use(request, undefined, () => {
       expect(request.user).toEqual(user);
-    });
-  });
-
-  it('should set no user to the request if it is not provided', async () => {
-    const request: any = {};
-
-    const middlewarefunction = middleware.resolve(undefined);
-
-    middlewarefunction(request, undefined, () => {
-      expect(request.user).toEqual(undefined);
     });
   });
 });
