2ed642e66ce02ebd41f0d2cf393cf8536e20331b
Marco <m.haemmerle@nemocontra.de>
2019-03-10 10:36:52 +0100

    fix tests

--- apps/nestjs-starter/src/app/authentication/authentication.controller.spec.ts
+++ apps/nestjs-starter/src/app/authentication/authentication.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationController } from './authentication.controller';
+import { AuthenticationService } from './authentication.service';
 
 describe('Authentication Controller', () => {
   let module: TestingModule;
@@ -7,6 +8,7 @@ describe('Authentication Controller', () => {
   beforeAll(async () => {
     module = await Test.createTestingModule({
       controllers: [AuthenticationController],
+      providers: [AuthenticationService]
     }).compile();
   });
   it('should be defined', () => {
